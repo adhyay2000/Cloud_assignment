2b78d7746f4d4553e98cfe10fe044926edfdaeb4
adhyay2000 <adityabundi2000@gmail.com>
2020-09-24 17:22:01 +0000

    Created the java servlet for serving post requests. Also added the folder structure.

---
+++ src/main/java/com/project/sqlservlet.java
@@ -0,0 +1,24 @@
+package com.sample;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServelt{
+	name="sql";
+	urlPatterns = "/sql";
+}
+public class sqlservlet extends HttpServelt{
+	@Override
+	protected void doPost(HttpServletRequest req,HttpServletResponse res) throws ServletException,IOException{
+		String SQL = req.getParameter("sql");
+		// PARSE SQL AND CONVERT TO JSON
+		// GET THE ANSWER IN result_json
+		req.setAttribute("results",result_json);
+		RequestDispatcher view = req.getRequestDispatcher("result.jsp");
+		view.forward(req,resp);
+	//doGet
+}	
---
+++ src/main/webapp/result.jsp
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<body>
+		<center>
+			<h1>RESULT:</h1>
+			<%
+			json ans = (json)request.getAttribute("result");
+			print json
+			%>
+	</body>
+</html>
+			
